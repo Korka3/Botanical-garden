4bce0561c398884f1689bc955996c48dfba1e97a
Korka3 <93039459+Korka3@users.noreply.github.com>
2022-07-05 13:24:44 +0200

    hamburger-menu

---
+++ scripts.js
@@ -0,0 +1,19 @@
+$(function(){
+  $(".c-nav__mobile").hide();
+
+
+  });
+
+  $(function(){
+    $(".menu-button").on('click',function(){$(".c-nav__mobile").toggleClass('c-nav__mobile--ontop')})
+    
+  });
+
+
+  // $('#toggle').on(function() {
+  //   $(this).toggleClass('active');
+  //   $('#overlay').toggleClass('open');
+  //  });
+
+  // zrbić klase dla menu displey block (dupa) usuną hide
+  // preventDefault uzyj
